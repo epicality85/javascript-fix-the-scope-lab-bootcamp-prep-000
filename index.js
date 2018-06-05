5fbbb93760928b1a24782793a6fb80070bb1ee7a
Daniel O'Brien <dannyk85.dob@gmail.com>
2018-06-05 15:04:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var animal = 'dog'
 
 function myAnimal() {
-  return animal
+  return animal // returns the global var animal
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  var animal = "cat"
+  var animal = "cat" // local var animal is returned
   return animal
 }
 
@@ -27,4 +27,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()
+theFunk
